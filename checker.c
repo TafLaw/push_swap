e4e4ecc0bf13ec8659dc841608ded7fd2dcce3d4
Tafadzwa MUZERENGANI <tmuzeren@c5r2s10.wethinkcode.co.za>
2019-07-09 17:07:58 +0200

    checker.c

--- checker.c
+++ checker.c
@@ -6,20 +6,38 @@
 /*   By: tmuzeren <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/08 16:43:22 by tmuzeren          #+#    #+#             */
-/*   Updated: 2019/07/08 18:03:09 by tmuzeren         ###   ########.fr       */
+/*   Updated: 2019/07/09 17:07:44 by tmuzeren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-checker(int *arr)
+int		length(t_list *root)
+{
+	t_list *temp;
+	int		len;
+
+	temp = root;
+	while (temp != NULL)
+	{
+		len++;
+		temp = temp -> link;
+	}
+	return (len);
+}
+
+void	checker(t_list *root)
 {
 	unsigned int i;
+	t_list		*temp;
+	
+	temp = root;
 
 	i = 0;
 	j = 0;
 	while (1)
 	{
+		if 
 		if (!(arr[i] % 2) || !(arr[i] % 3) || arr[i])
 			i++;
 	}
