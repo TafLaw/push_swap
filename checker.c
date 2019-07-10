129616d478b606b0febb0d9e06414f1505964c68
Tafadzwa MUZERENGANI <tmuzeren@c5r2s10.wethinkcode.co.za>
2019-07-10 14:40:10 +0200

    checker.c

--- checker.c
+++ checker.c
@@ -6,18 +6,20 @@
 /*   By: tmuzeren <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/08 16:43:22 by tmuzeren          #+#    #+#             */
-/*   Updated: 2019/07/09 17:07:44 by tmuzeren         ###   ########.fr       */
+/*   Updated: 2019/07/10 11:12:17 by tmuzeren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "push_swap.h"
+#include <stdio.h>
 
-int		length(t_list *root)
+int		length(struct node *r)
 {
-	t_list *temp;
+	struct node *temp;
 	int		len;
 
-	temp = root;
+	temp = r;
+	len = 0;
 	while (temp != NULL)
 	{
 		len++;
@@ -26,16 +28,52 @@ int		length(t_list *root)
 	return (len);
 }
 
-void	checker(t_list *root)
+int		duplicate(int *arr)
 {
-	unsigned int i;
-	t_list		*temp;
-	
-	temp = root;
+	int i;
+	int j;
+	int dup;
 
 	i = 0;
-	j = 0;
-	while (1)
+	dup = 0;
+	j=0;
+	while (i < 7)
+	{
+		//j = i + 1;
+		while (j + 1 < 7)
+		{
+			if (arr[i] == arr[i+1])
+				return (1);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+void	checker(struct node *r)
+{
+	int			i;
+	struct node	*temp;
+	
+	temp = r;
+
+	i = 0;
+	//j = 0;
+	if (!temp)
+		return;
+	while (i < length(temp))
+	{
+		if (ft_isdigit(temp -> data) || duplicate((int *)temp) || 
+				temp -> data > MAX)
+		{
+			printf("Error\n");
+			return;
+		}
+		temp = temp -> link;
+		i++;
+	}
+	/*while (1)
 	{
 		if 
 		if (!(arr[i] % 2) || !(arr[i] % 3) || arr[i])
@@ -44,14 +82,70 @@ void	checker(t_list *root)
 	while (j < i)
 	{
 		if (arr[i])
+	//}*/
+}
+
+void append()
+{
+	struct node *p;
+	struct node *temp;
+
+	temp = (struct node *)malloc(sizeof(struct node));
+	scanf("%d", &temp -> data);
+	temp -> link = NULL;
+
+	if (root ==	NULL)
+		root = temp;
+	else
+	{	
+		p = root;
+		while (p -> link != NULL)
+		{
+			p = p -> link;
+		}
+		p -> link = temp;
 	}
 }
 
-int		main(int argc, char *argv[])
+void display()
 {
-	if (argc != 2)
-		return (0);
+	struct node *temp;
+	
+	temp = root;
+	if (temp == NULL)
+		printf ("ZERO\n");
 	else
-		checker()
+	{
+		while (temp != NULL)
+		{
+			printf("%d -> ", temp -> data);
+			temp = temp -> link;
+		}
+		printf("NULL\n");
+	}
+}
 
+int		main(void)
+{
+	int choice;
+
+	while (1)
+	{
+		printf("\nchoice : ");
+		scanf("%d", &choice);
+
+		if (choice == 1)
+			append();
+		else if (choice == 2)
+			display();
+		else if (choice == 3)
+			printf("%d\n", length(root));
+		else if (choice == 4)
+			printf("%d Duplicates\n", duplicate((int *)root));
+		else if (choice == 5)
+			checker(root);
+		else
+			return (0);
+	}
+	return (0);
 }
