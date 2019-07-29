93a4a313c15b6b5586392193df962866ef525f5c
Tafadzwa MUZERENGANI <tmuzeren@c5r2s10.wethinkcode.co.za>
2019-07-29 16:50:38 +0200

    sort

--- sort.c
+++ sort.c
@@ -6,13 +6,13 @@
 /*   By: tmuzeren <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/19 09:42:25 by tmuzeren          #+#    #+#             */
-/*   Updated: 2019/07/25 12:15:45 by tmuzeren         ###   ########.fr       */
+/*   Updated: 2019/07/25 13:15:39 by tmuzeren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	sort_3(struct node **stack)
+void	sort_3(struct node **stack)//Sorting only 3 numbers & working fine
 {
 	struct node *temp;
 
@@ -41,4 +41,23 @@ void	sort_3(struct node **stack)
 	}
 	else
 		rrot_ab(stack);
+}//This might change in due time
+
+void	sort_5(struct node **stack, struct node **b)
+{
+	struct node *temp;
+	int i;
+
+	i = 1;
+	temp = *stack;
+	while (i++ <= 2)
+		*b = push_b(stack, *b);
+	sort_3(stack);
+	*stack = push_a(b, *stack);
+	rot_ab(stack);
+	*stack = push_a(b, *stack);
+	sort_3(stack);
+	 while (i++ <= 2)
+  		 *b = push_b(stack, *b);
+	 sort_3(stack);
 }
